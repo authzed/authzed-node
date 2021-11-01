32583ad7227ec7cdf21bc7e09f18859e6187ab32
Jacobus Meulen <sjaakiejj@gmail.com>
2021-11-01 08:25:59 +0700

    Setup v1

--- src/full.test.ts
+++ src/full.test.ts
@@ -1,12 +1,14 @@
 import * as v0 from "./v0";
-import * as v1 from "./v1alpha1";
+import * as v1 from "./v1";
+import * as v1alpha from "./v1alpha1";
 
 describe("a check following a write of schema and relationships", () => {
   it("should succeed", (done) => {
     // Write the schema.
+    const alphaClient = v1alpha.NewClient("sometoken", "localhost:50051", true);
     const v1client = v1.NewClient("sometoken", "localhost:50051", true);
 
-    const writeSchemaRequest = v1.WriteSchemaRequest.create({
+    const writeSchemaRequest = v1alpha.WriteSchemaRequest.create({
       schema: `
   definition test/user {}
   
@@ -17,72 +19,56 @@ describe("a check following a write of schema and relationships", () => {
   `,
     });
 
-    v1client.writeSchema(writeSchemaRequest, function (err, response) {
+    alphaClient.writeSchema(writeSchemaRequest, function (err) {
       expect(err).toBe(null);
-      expect(response?.objectDefinitionsNames).toEqual([
-        "test/user",
-        "test/resource",
-      ]);
-
-      const v0client = v0.NewClient("sometoken", "localhost:50051", true);
 
       // Create the relationship between the resource and the user.
-      const resourceViewerRelation = v0.ObjectAndRelation.create({
-        namespace: "test/resource",
+      const resource = v1.ObjectReference.create({
+        objectType: "test/resource",
         objectId: "someresource",
-        relation: "viewer",
       });
 
       // Create the user reference.
-      const userref = v0.ObjectAndRelation.create({
-        namespace: "test/user",
+      const userref = v1.ObjectReference.create({
+        objectType: "test/user",
         objectId: "someuser",
-        relation: "...",
       });
 
-      const user = v0.User.create({
-        userOneof: {
-          userset: userref,
-          oneofKind: "userset",
-        },
+      const user = v1.SubjectReference.create({
+        object: userref,
       });
 
-      const relationship = v0.RelationTuple.create({
-        objectAndRelation: resourceViewerRelation,
-        user,
+      const relationship = v1.Relationship.create({
+        resource,
+        relation: "viewer",
+        subject: user,
       });
 
-      const update = v0.RelationTupleUpdate.create({
-        operation: v0.RelationTupleUpdate_Operation.CREATE,
-        tuple: relationship,
+      const update = v1.RelationshipUpdate.create({
+        operation: v1.RelationshipUpdate_Operation.CREATE,
+        relationship: relationship,
       });
 
       // Write the relationship.
-      const writeRequest = v0.WriteRequest.create({
+      const writeRequest = v1.WriteRelationshipsRequest.create({
         updates: [update],
       });
 
-      v0client.write(writeRequest, function (err, response) {
+      v1client.writeRelationships(writeRequest, function (err, response) {
         expect(err).toBe(null);
         expect(response).toBeTruthy();
-        const revision = response?.revision;
 
-        // Check and verify.
-        const viewResourcePermission = v0.ObjectAndRelation.create({
-          namespace: "test/resource",
-          objectId: "someresource",
-          relation: "view",
+        const checkPermissionRequest = v1.CheckPermissionRequest.create({
+          resource,
+          permission: "view",
+          subject: user,
         });
 
-        const checkRequest = v0.CheckRequest.create({
-          testUserset: viewResourcePermission,
-          user,
-          atRevision: revision,
-        });
-
-        v0client.check(checkRequest, function (err, response) {
+        v1client.checkPermission(checkPermissionRequest, (err, response) => {
           expect(err).toBe(null);
-          expect(response?.membership).toBe(v0.CheckResponse_Membership.MEMBER);
+          expect(response?.permissionship).toBe(
+            v1.CheckPermissionResponse_Permissionship.HAS_PERMISSION
+          );
           done();
         });
       });
