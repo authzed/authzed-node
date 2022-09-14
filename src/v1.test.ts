eab78429d79d9b4970a72e3acacf3f698454158d
Sam Kim <sam@authzed.com>
2022-09-13 22:32:54 -0700

    Add lookup api tests

--- src/v1.test.ts
+++ src/v1.test.ts
@@ -13,32 +13,36 @@ import {
   RelationshipUpdate_Operation,
   CheckPermissionResponse,
   WriteRelationshipsResponse,
-} from "./v1";
-import * as grpc from "@grpc/grpc-js";
+  LookupSubjectsRequest,
+  LookupSubjectsResponse,
+  LookupResourcesRequest,
+  LookupResourcesResponse,
+} from './v1';
+import * as grpc from '@grpc/grpc-js';
 
-describe("a check with an unknown namespace", () => {
-  it("should raise a failed precondition", (done) => {
+describe('a check with an unknown namespace', () => {
+  it('should raise a failed precondition', (done) => {
     const resource = ObjectReference.create({
-      objectType: "test/somenamespace",
-      objectId: "bar",
+      objectType: 'test/somenamespace',
+      objectId: 'bar',
     });
 
     const testUser = ObjectReference.create({
-      objectType: "test/user",
-      objectId: "someuser",
+      objectType: 'test/user',
+      objectId: 'someuser',
     });
 
     const checkPermissionRequest = CheckPermissionRequest.create({
       resource,
-      permission: "someperm",
+      permission: 'someperm',
       subject: SubjectReference.create({
         object: testUser,
       }),
     });
 
     const client = NewClient(
-      "v1-failed-sometoken",
-      "localhost:50051",
+      'v1-failed-sometoken',
+      'localhost:50051',
       ClientSecurity.INSECURE_LOCALHOST_ALLOWED
     );
     client.checkPermission(checkPermissionRequest, function (err, response) {
@@ -49,12 +53,12 @@ describe("a check with an unknown namespace", () => {
   });
 });
 
-describe("a check with an known namespace", () => {
-  it("should succeed", (done) => {
+describe('a check with an known namespace', () => {
+  it('should succeed', (done) => {
     // Write some schema.
     const client = NewClient(
-      "v1-sometoken",
-      "localhost:50051",
+      'v1-sometoken',
+      'localhost:50051',
       ClientSecurity.INSECURE_LOCALHOST_ALLOWED
     );
 
@@ -80,13 +84,13 @@ describe("a check with an known namespace", () => {
         return new Promise((resolve) => {
           // Write a relationship.
           const resource = ObjectReference.create({
-            objectType: "test/document",
-            objectId: "somedocument",
+            objectType: 'test/document',
+            objectId: 'somedocument',
           });
 
           const testUser = ObjectReference.create({
-            objectType: "test/user",
-            objectId: "someuser",
+            objectType: 'test/user',
+            objectId: 'someuser',
           });
 
           const writeRequest = WriteRelationshipsRequest.create({
@@ -94,7 +98,7 @@ describe("a check with an known namespace", () => {
               RelationshipUpdate.create({
                 relationship: Relationship.create({
                   resource: resource,
-                  relation: "viewer",
+                  relation: 'viewer',
                   subject: SubjectReference.create({
                     object: testUser,
                   }),
@@ -122,13 +126,13 @@ describe("a check with an known namespace", () => {
           // Call check.
           const checkPermissionRequest = CheckPermissionRequest.create({
             resource,
-            permission: "view",
+            permission: 'view',
             subject: SubjectReference.create({
               object: testUser,
             }),
             consistency: Consistency.create({
               requirement: {
-                oneofKind: "fullyConsistent",
+                oneofKind: 'fullyConsistent',
                 fullyConsistent: true,
               },
             }),
@@ -151,5 +155,149 @@ describe("a check with an known namespace", () => {
 
         done();
       });
+  });
+});
+
+describe('Lookup APIs', () => {
+  let token: string;
+  beforeEach(async () => {
+    token = `v1-lookupsubject-${Math.floor(Math.random() * 1000)}`;
+    const client = NewClient(
+      token,
+      'localhost:50051',
+      ClientSecurity.INSECURE_LOCALHOST_ALLOWED
+    );
+
+    const request = WriteSchemaRequest.create({
+      schema: `definition test/user {}
+
+      definition test/document {
+        relation viewer: test/user
+        permission view = viewer
+      }
+      `,
+    });
+
+    await new Promise((resolve) => {
+      client.writeSchema(request, function (err, response) {
+        expect(err).toBe(null);
+        resolve(response);
+      });
+    });
+
+    const resource = ObjectReference.create({
+      objectType: 'test/document',
+      objectId: 'somedocument',
+    });
+
+    const writeRequest = WriteRelationshipsRequest.create({
+      updates: [
+        RelationshipUpdate.create({
+          relationship: Relationship.create({
+            resource: resource,
+            relation: 'viewer',
+            subject: SubjectReference.create({
+              object: ObjectReference.create({
+                objectType: 'test/user',
+                objectId: 'someuser',
+              }),
+            }),
+          }),
+          operation: RelationshipUpdate_Operation.CREATE,
+        }),
+        RelationshipUpdate.create({
+          relationship: Relationship.create({
+            resource: resource,
+            relation: 'viewer',
+            subject: SubjectReference.create({
+              object: ObjectReference.create({
+                objectType: 'test/user',
+                objectId: 'someuser2',
+              }),
+            }),
+          }),
+          operation: RelationshipUpdate_Operation.CREATE,
+        }),
+      ],
+    });
+
+    await new Promise((resolve) => {
+      client.writeRelationships(writeRequest, function (err, response) {
+        expect(err).toBe(null);
+        resolve(response);
+      });
+    });
+  });
+
+  it('can lookup subjects', (done) => {
+    const client = NewClient(
+      token,
+      'localhost:50051',
+      ClientSecurity.INSECURE_LOCALHOST_ALLOWED
+    );
+
+    const request = LookupSubjectsRequest.create({
+      resource: ObjectReference.create({
+        objectType: 'test/document',
+        objectId: 'somedocument',
+      }),
+      permission: 'view',
+      subjectObjectType: 'test/user',
+    });
+
+    const resStream = client.lookupSubjects(request);
+
+    resStream.on('data', function (subject: LookupSubjectsResponse) {
+      expect(['someuser', 'someuser2']).toContain(subject.subjectObjectId);
+    });
+
+    resStream.on('end', function () {
+      done();
+    });
+
+    resStream.on('error', function (e) {
+      done.fail(e);
+    });
+
+    resStream.on('status', function (status) {
+      expect(status.code).toEqual(grpc.status.OK);
+    });
+  });
+
+  it('can lookup resources', (done) => {
+    const client = NewClient(
+      token,
+      'localhost:50051',
+      ClientSecurity.INSECURE_LOCALHOST_ALLOWED
+    );
+
+    const request = LookupResourcesRequest.create({
+      subject: SubjectReference.create({
+        object: ObjectReference.create({
+          objectType: 'test/user',
+          objectId: 'someuser',
+        }),
+      }),
+      permission: 'view',
+      resourceObjectType: 'test/document',
+    });
+
+    const resStream = client.lookupResources(request);
+
+    resStream.on('data', function (response: LookupResourcesResponse) {
+      expect(response.resourceObjectId).toEqual('somedocument');
+    });
+
+    resStream.on('end', function () {
+      done();
+    });
+
+    resStream.on('error', function (e) {
+      done.fail(e);
+    });
+
+    resStream.on('status', function (status) {
+      expect(status.code).toEqual(grpc.status.OK);
+    });
   });
 });
