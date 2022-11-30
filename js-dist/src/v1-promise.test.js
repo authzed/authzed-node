f940466b54b48aaf4bdbb2c8ef102b215dca7174
Sam Kim <sam@authzed.com>
2022-11-29 23:41:38 -0800

    Add caveat check tests

--- js-dist/src/v1-promise.test.js
+++ js-dist/src/v1-promise.test.js
@@ -22,6 +22,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const v1_1 = require("./v1");
 const grpc = __importStar(require("@grpc/grpc-js"));
 const helpers_1 = require("./__utils__/helpers");
+const struct_1 = require("./authzedapi/google/protobuf/struct");
 describe('a check with an unknown namespace', () => {
     it('should raise a failed precondition', async () => {
         var _a;
@@ -115,6 +116,108 @@ describe('a check with an known namespace', () => {
         const checkResponse = await client.checkPermission(checkPermissionRequest, new grpc.Metadata(), {});
         expect(checkResponse === null || checkResponse === void 0 ? void 0 : checkResponse.permissionship).toBe(v1_1.CheckPermissionResponse_Permissionship.HAS_PERMISSION);
         client.close();
+    });
+    describe('with caveated relations', () => {
+        it('should succeed', async () => {
+            // Write some schema.
+            const { promises: client } = v1_1.NewClient(helpers_1.generateTestToken('v1-promise-caveats'), 'localhost:50051', v1_1.ClientSecurity.INSECURE_LOCALHOST_ALLOWED);
+            const schemaRequest = v1_1.WriteSchemaRequest.create({
+                schema: `definition test/user {}
+
+          caveat has_special_attribute(special bool) {
+            special == true
+          }
+
+          definition test/document {
+            relation viewer: test/user
+            relation caveated_viewer: test/user with has_special_attribute
+            permission view = viewer + caveated_viewer
+          }
+      `,
+            });
+            const schemaResponse = await client.writeSchema(schemaRequest, new grpc.Metadata(), {});
+            expect(schemaResponse).toBeTruthy();
+            // Write a relationship.
+            const resource = v1_1.ObjectReference.create({
+                objectType: 'test/document',
+                objectId: 'somedocument',
+            });
+            const testUser = v1_1.ObjectReference.create({
+                objectType: 'test/user',
+                objectId: 'specialuser',
+            });
+            const writeRequest = v1_1.WriteRelationshipsRequest.create({
+                updates: [
+                    v1_1.RelationshipUpdate.create({
+                        relationship: v1_1.Relationship.create({
+                            resource: resource,
+                            relation: 'caveated_viewer',
+                            subject: v1_1.SubjectReference.create({
+                                object: testUser,
+                            }),
+                            optionalCaveat: v1_1.ContextualizedCaveat.create({
+                                caveatName: 'has_special_attribute',
+                            }),
+                        }),
+                        operation: v1_1.RelationshipUpdate_Operation.CREATE,
+                    }),
+                ],
+            });
+            const response = await client.writeRelationships(writeRequest, new grpc.Metadata(), {});
+            expect(response).toBeTruthy();
+            // Call check when user has special attribute.
+            let checkPermissionRequest = v1_1.CheckPermissionRequest.create({
+                resource,
+                permission: 'view',
+                subject: v1_1.SubjectReference.create({
+                    object: testUser,
+                }),
+                consistency: v1_1.Consistency.create({
+                    requirement: {
+                        oneofKind: 'fullyConsistent',
+                        fullyConsistent: true,
+                    },
+                }),
+                context: struct_1.Struct.fromJson({ special: true }),
+            });
+            let checkResponse = await client.checkPermission(checkPermissionRequest, new grpc.Metadata(), {});
+            expect(checkResponse === null || checkResponse === void 0 ? void 0 : checkResponse.permissionship).toBe(v1_1.CheckPermissionResponse_Permissionship.HAS_PERMISSION);
+            // Call check when user does not have the special attribute.
+            checkPermissionRequest = v1_1.CheckPermissionRequest.create({
+                resource,
+                permission: 'view',
+                subject: v1_1.SubjectReference.create({
+                    object: testUser,
+                }),
+                consistency: v1_1.Consistency.create({
+                    requirement: {
+                        oneofKind: 'fullyConsistent',
+                        fullyConsistent: true,
+                    },
+                }),
+                context: struct_1.Struct.fromJson({ special: false }),
+            });
+            checkResponse = await client.checkPermission(checkPermissionRequest, new grpc.Metadata(), {});
+            expect(checkResponse === null || checkResponse === void 0 ? void 0 : checkResponse.permissionship).toBe(v1_1.CheckPermissionResponse_Permissionship.NO_PERMISSION);
+            // Call check when user's special attribute is unspecified.
+            checkPermissionRequest = v1_1.CheckPermissionRequest.create({
+                resource,
+                permission: 'view',
+                subject: v1_1.SubjectReference.create({
+                    object: testUser,
+                }),
+                consistency: v1_1.Consistency.create({
+                    requirement: {
+                        oneofKind: 'fullyConsistent',
+                        fullyConsistent: true,
+                    },
+                }),
+                context: {},
+            });
+            checkResponse = await client.checkPermission(checkPermissionRequest, new grpc.Metadata(), {});
+            expect(checkResponse === null || checkResponse === void 0 ? void 0 : checkResponse.permissionship).toBe(v1_1.CheckPermissionResponse_Permissionship.CONDITIONAL_PERMISSION);
+            client.close();
+        });
     });
 });
 describe('Lookup APIs', () => {
