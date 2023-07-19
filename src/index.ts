8e7f9751de5384c445b00da8b11a0f397ae15807
Sam Kim <sam@authzed.com>
2023-07-18 17:15:09 -0700

    Re-export vendored protobuf definitions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 "use strict";
 
-export * as v1 from "./v1";
+export * as protobuf from './protobuf';
+export * as v1 from './v1';
---
+++ src/protobuf.ts
@@ -0,0 +1,4 @@
+export * from './authzedapi/google/protobuf/descriptor';
+export * from './authzedapi/google/protobuf/duration';
+export * from './authzedapi/google/protobuf/struct';
+export * from './authzedapi/google/protobuf/timestamp';
